0d7e14726b16740f5f6fc3c99a6135527220995c
Bryan <btjosueturcios@gmail.com>
2025-05-06 14:54:20 +0200

    add-env-and-cd

--- include/builtins.h
+++ include/builtins.h
@@ -3,18 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-mens <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: brturcio <brturcio@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 10:18:42 by brturcio          #+#    #+#             */
-/*   Updated: 2025/05/03 10:23:54 by yel-mens         ###   ########.fr       */
+/*   Updated: 2025/05/06 14:46:38 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
+# include "structs.h"
 # include "minishell.h"
 
-int	echo_builtins(char **cmd);
+# define PATH_MAX 4096
+
+int		ft_echo_builtins(t_cmd *cmd);
+int		ft_pwd_builtins(t_cmd *cmd);
+int		ft_env_builtins(t_cmd *cmd, t_env *env);
+int		ft_cd_builtins(t_cmd *cmd, t_shell *shell);
+
+char	*ft_get_val(char *var, t_shell *shell);
+char	*ft_update_env(char *var, char *data, t_shell *shell);
+int		ft_cd_path(char *arg, t_shell *shell);
 
 #endif
